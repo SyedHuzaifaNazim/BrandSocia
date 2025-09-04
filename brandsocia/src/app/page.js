fab1b3ce416887c05be557e0f1a3f9c8900fd4ea
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2025-09-04 20:54:01 +0500

    modified

--- brandsocia/src/app/page.js
+++ brandsocia/src/app/page.js
@@ -5,11 +5,15 @@ import PortfolioPreview from '@/components/sections/PortfolioPreview'
 import BlogPreview from '@/components/sections/BlogPreview'
 import ContactCTA from '@/components/sections/ContactCTA'
 import Footer from '@/components/layout/Footer'
+import ColorGradientBackground from '@/components/ColorGradientBackground'
+import AnimatedGradientBackground from '@/components/AnimatedGradientBackground'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ColorGradientBackground />
+      <AnimatedGradientBackground />
       <Hero />
       <ServicesSection />
       <PortfolioPreview />
