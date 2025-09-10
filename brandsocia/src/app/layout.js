b680852a278caa1427868eb5c724495ac104249a
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2025-09-10 13:13:54 +0500

    layout modified with favicon

--- brandsocia/src/app/layout.js
+++ brandsocia/src/app/layout.js
@@ -14,11 +14,26 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "BrandSocia",
   description: "Develop the Future!",
+  icons: {
+    icon: [
+      {
+        url: '/WhatsApp_Image_2025-07-01_at_17.12.43_38099a7e-removebg-preview.png',
+        sizes: 'any',
+        type: 'image/png',
+      },
+    ],
+    shortcut: '/WhatsApp_Image_2025-07-01_at_17.12.43_38099a7e-removebg-preview.png',
+    apple: '/WhatsApp_Image_2025-07-01_at_17.12.43_38099a7e-removebg-preview.png',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/WhatsApp_Image_2025-07-01_at_17.12.43_38099a7e-removebg-preview.png" />
+        <link rel="apple-touch-icon" href="/WhatsApp_Image_2025-07-01_at_17.12.43_38099a7e-removebg-preview.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
