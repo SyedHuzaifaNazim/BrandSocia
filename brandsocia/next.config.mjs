db32b8c24249adc5c9823e3c18fbf879c1e0da33
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2025-09-13 14:08:35 +0500

    initial website done

--- brandsocia/next.config.mjs
+++ brandsocia/next.config.mjs
@@ -4,7 +4,9 @@ const nextConfig = {
     // appDir: true,
   },
   images: {
-    domains: ['brandsocia.com', 'via.placeholder.com', 'images.unsplash.com','img.freepik.com','aceternity.com','logoipsum.com','static.thenounproject.com'],
+    domains: ['brandsocia.com', 'via.placeholder.com', 'images.unsplash.com','img.freepik.com','aceternity.com','logoipsum.com','static.thenounproject.com', 'images.pexels.com',
+      'cdn.pixabay.com',
+      'burst.shopifycdn.com'],
     unoptimized: true, // Add this if you're having image optimization issues
   },
 }
